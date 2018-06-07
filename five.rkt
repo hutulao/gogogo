7ae6824fb2a3360be28cf131362a92a1ba77cfd8
hutulao <hutusheng6666666@163.com>
2018-06-07 22:18:08 +0800

    go

--- five.rkt
+++ five.rkt
@@ -3,16 +3,24 @@
 (require 2htdp/universe)
 
 ; constant
-(define HEIGHT 200) ; the height of the BACKGROUND
-(define WIDTH 200) ; its width
+(define DIAMETER 4)
+(define HEIGHT (* DIAMETER 300)) ; the height of the BACKGROUND
+(define WIDTH (* DIAMETER 400)) ; its width
+
 
 ; graphical constant
 (define BACKGROUND (empty-scene WIDTH HEIGHT))
-(define FOOD (circle 5 "solid" "green"))
-(define WORM (circle 5 "solid" "red"))
+(define FOOD (circle DIAMETER "solid" "green"))
+(define WORM (circle DIAMETER "solid" "red"))
 
 ; struct
 (define-struct POS [x y])
+(define-struct VEL [dx dy])
+(define-struct worm [pos vel])
+
+; LWorms is one of 
+; '()
+; (cons worm LWorms)
 
 
 ; direct is a procedure
@@ -23,25 +31,46 @@
 ; down make worm turn down
 (define (direct w a-key)
   (cond
-    [(key=? a-key "left") w]
-    [(key=? a-key "right") w]
-    [(key=? a-key "up") w]
-    [(key=? a-key "down") w]
+    [(and (key=? a-key "left") (not (= (VEL-dx (worm-vel w)) DIAMETER)))
+     (make-worm (worm-pos w) (make-VEL (- DIAMETER) 0))]
+    [(and (key=? a-key "right") (not (= (VEL-dx (worm-vel w)) (- DIAMETER))))
+     (make-worm (worm-pos w) (make-VEL DIAMETER 0))]
+    [(and (key=? a-key "up") (not (= (VEL-dy (worm-vel w)) DIAMETER)))
+     (make-worm (worm-pos w) (make-VEL 0 (- DIAMETER)))]
+    [(and (key=? a-key "down") (not (= (VEL-dy (worm-vel w)) (- DIAMETER))))
+     (make-worm (worm-pos w) (make-VEL 0 DIAMETER))]
     [else w]))
 
 ; worm make one step every tick 
-(define (tock w) w)
+(define (tock w)
+  (make-worm (make-POS (+ (VEL-dx (worm-vel w)) (POS-x (worm-pos w)))
+                       (+ (VEL-dy (worm-vel w)) (POS-y (worm-pos w))))
+             (worm-vel w)))
 
 ; render the world on the BACKGROUND
-(define (render w) w)
+(define (render w)
+  (place-image WORM (POS-x (worm-pos w)) (POS-y (worm-pos w)) BACKGROUND))
+;(render (make-worm (make-POS 100 190) (make-VEL 0 0)))
 
 ; judge the game is over or not
-(define (stop? w) w)
+(define (stop? w)
+  (cond
+    [(< (POS-x (worm-pos w)) 0) #t]
+    [(> (POS-x (worm-pos w)) WIDTH) #t]
+    [(> (POS-y (worm-pos w)) HEIGHT) #t]
+    [(< (POS-y (worm-pos w)) 0) #t]
+    [else #f]))
+
+; render the last world
+(define (last-render w)
+  (overlay/align "middle" "bottom" (text "game over!" 24 "olive")
+                 (place-image WORM (POS-x (worm-pos w)) (POS-y (worm-pos w)) BACKGROUND)))
 
 ; a worm and food game
-(define (Worm s)
+(define (worm-main s)
   (big-bang s
     (on-key direct)
     (on-tick tock)
     (to-draw render)
-    (stop-when stop?)))
+    (stop-when stop? last-render)))
+(worm-main (make-worm (make-POS 400 400) (make-VEL 0 0)))
